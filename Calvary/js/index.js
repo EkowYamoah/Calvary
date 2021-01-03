32813645d050f7e67e4487f950002328b84d5f2f
EkowYamoah <42788503+EkowYamoah@users.noreply.github.com>
2021-01-03 02:23:09 +0000

    Add files via upload

--- Calvary/js/index.js
+++ Calvary/js/index.js
@@ -9,5 +9,15 @@ $toggleCollapse.click(function(){
     $nav.toggleClass('collapse');
 })
 
+//Owl Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+});
+
 });
 
